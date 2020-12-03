87c0d061127d72a96ad18359377b3353832451ab
Perry Defayette <defayetteperry@gmail.com>
2020-12-03 18:56:29 +0000

    Updated packinglist.js with while loop.

--- packing-list.js
+++ packing-list.js
@@ -5,3 +5,11 @@ console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+console.log("\n");
+
+let i = 0;
+while(i < packingList.length){
+	console.log(packingList[i]);
+	i++;
+}
